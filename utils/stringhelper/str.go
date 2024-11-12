60d06a662d7c480fb999f7671b677d66ee0d0e8c
chenhao <chenhao@matamaker.cn>
2024-11-12 15:58:09 +0800

    1，修正逻辑错误

--- utils/stringhelper/str.go
+++ utils/stringhelper/str.go
@@ -9,16 +9,9 @@ func ConvertCamelToSnake(camel string, joinStr string) string {
 	if len(joinStr) == 0 {
 		joinStr = "_"
 	}
-	str := regexp.MustCompile(`([a-z])([A-Z])`).ReplaceAllString(camel, `$1`+joinStr+`$2`)
+	replaceStr := `${1}` + joinStr + `${2}`
+	str := regexp.MustCompile(`([a-z])([A-Z])`).ReplaceAllString(camel, replaceStr)
 	return strings.ToLower(str)
-	//var snake []string
-	//for _, word := range regexp.MustCompile(`([a-z])([A-Z])`).ReplaceAllString(camel, `$1_$2`) {
-	//	snake = append(snake, strings.ToLower(string(word)))
-	//}
-	//if len(joinStr) == 0 {
-	//	joinStr = "_"
-	//}
-	//return strings.Join(snake, joinStr)
 }
 
 func ConvertCamelToSnakeWithDefault(camel string) string {
