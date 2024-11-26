841ebabffc8703c1351fbc6cacd8a1d5abedfb28
chenhao <chenhao@matamaker.cn>
2024-11-26 11:43:59 +0800

    1，修正日志参数错误

--- utils/loghelper/log.go
+++ utils/loghelper/log.go
@@ -32,7 +32,7 @@ func (log *LogManager) Debug(message any) {
 	logc.Debugv(log.ctx, message)
 }
 func (log *LogManager) DebugFormat(messageFormat string, args ...any) {
-	logc.Debugf(log.ctx, messageFormat, args)
+	logc.Debugf(log.ctx, messageFormat, args...)
 }
 
 func (log *LogManager) Info(message any) {
@@ -40,12 +40,12 @@ func (log *LogManager) Info(message any) {
 }
 
 func (log *LogManager) InfoFormat(messageFormat string, args ...any) {
-	logc.Infof(log.ctx, messageFormat, args)
+	logc.Infof(log.ctx, messageFormat, args...)
 }
 func (log *LogManager) Error(message any) {
 	logc.Errorv(log.ctx, message)
 }
 
 func (log *LogManager) ErrorFormat(messageFormat string, args ...any) {
-	logc.Errorf(log.ctx, messageFormat, args)
+	logc.Errorf(log.ctx, messageFormat, args...)
 }
