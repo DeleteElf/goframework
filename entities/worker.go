7a3fbcef273bba45a1c2ef10f74358f824fe93e0
chenhao <48207475@qq.com>
2025-06-16 10:35:33 +0800

    1,优化工作者流程

--- entities/worker.go
+++ entities/worker.go
@@ -2,9 +2,9 @@ package entities
 
 // IWorker 工作者接口
 type IWorker interface {
-	Init(config IConfig) error
-	Start()
-	Stop()
+	Init(config IConfig) bool
+	Start() bool
+	Stop() bool
 }
 
 // IRunWorker 需要运行的工作者接口
